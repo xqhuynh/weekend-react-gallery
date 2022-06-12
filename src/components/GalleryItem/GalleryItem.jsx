f56243cb495b289173bc0a2164c64bbb061fd62e
Savon Huynh <xqhuynh@gmail.com>
2022-06-12 16:06:57 -0500

    Create PUT and rendor items

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,5 +1,53 @@
-function GalleryItem() {
-  return <>Return Gallery Items</>;
+import React, { useState } from "react";
+import axios from "axios";
+// Retrieve 'item' and fetchPhotoItem props from GalleryList component
+function GalleryItem({ item, fetchPhotoItem }) {
+  // useState to toggle between image and description
+  const [displayPhoto, setDisplayPhoto] = useState(true);
+
+  // Function to toggle between image and description
+  // Set toggle to opposite of useState initial value
+  function togglePhoto() {
+    setDisplayPhoto(!displayPhoto);
+  }
+
+  // Axios PUT to track 'likes' button click and update to database
+  function addLikes() {
+    console.log("Clicked gallery item", item.id);
+    axios
+      .put(`/gallery/like/${item.id}`) // Target item.id, make sure key={item.id} in GalleryList
+      .then((response) => {
+        console.log("In PUT /gallery/like", response);
+        // console.log("Number of likes", item);
+        // Refresh data by calling fetchPhotoItem()
+        fetchPhotoItem();
+      })
+      .catch((err) => {
+        console.log("Error in PUT /gallery/like", err);
+      });
+  }
+
+  return (
+    <>
+      <div>
+        {/* Conditional rendering to toggle disply of image and description */}
+        {displayPhoto ? (
+          <img onClick={togglePhoto} src={item.path} />
+        ) : (
+          <p onClick={togglePhoto}>{item.description}</p>
+        )}
+
+        {/* 'Love it' button */}
+        <button onClick={addLikes}>Love It!</button>
+        {/* Conditional rendering for 'Love It' button */}
+        {item.likes !== 0 ? (
+          <p>{item.likes} people love this!</p>
+        ) : (
+          <p>No people love this!</p>
+        )}
+      </div>
+    </>
+  );
 }
 
 export default GalleryItem;
--- src/components/GalleryList/GalleryList.jsx
+++ src/components/GalleryList/GalleryList.jsx
@@ -1,10 +1,13 @@
 // Import GalleryItem component
 import GalleryItem from "../GalleryItem/GalleryItem";
 
-// Pass galleryItems prop from App.js, remember curly braces
+// Retrieve galleryItems prop from App.js, remember curly braces
 function GalleryList({ galleryItems, fetchPhotoItem }) {
   return (
     <>
+      {/* Loop over galleryItems and individual items
+          and pass props to GalleryItem component.
+          Set key={item.id} to use for axios PUT*/}
       <section>
         {galleryItems.map((item) => (
           <GalleryItem
